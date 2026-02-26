ee3e2e6982d930c561bb7240a8828a0c28a28084
Farhan Shahriar Haque <fsh33330@gmail.com>
2026-02-26 11:38:10 +0600

    ShoppingCart.java refactored

--- src/edu/iutcs/cr/ShoppingCart.java
+++ src/edu/iutcs/cr/ShoppingCart.java
@@ -4,8 +4,7 @@ import edu.iutcs.cr.system.SystemDatabase;
 import edu.iutcs.cr.vehicles.Vehicle;
 import java.io.Serializable;
 import java.util.HashSet;
-import static java.util.Objects.isNull;
-import java.util.Scanner;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -17,6 +16,9 @@ public class ShoppingCart implements Serializable {
     private final Set<Vehicle> vehicles;
     private final SystemDatabase database;
 
+    /**
+     * Constructor for creating a new shopping cart
+     */
     public ShoppingCart() {
         this.vehicles = new HashSet<>();
         database = SystemDatabase.getInstance();
@@ -25,40 +27,69 @@ public class ShoppingCart implements Serializable {
     public Set<Vehicle> getVehicles() {
         return this.vehicles;
     }
-//Resource Leaks (Scnanners not closed)
-    public void addItem() {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter registration number of vehicle: ");
-        String registrationNumber = scanner.next();
+    /**
+     * Add a vehicle to the cart by registration number
+     * 
+     * @param registrationNumber the registration number of the vehicle
+     * @return true if item was added, false if vehicle not available or not found
+     */
+    public boolean addItem(String registrationNumber) {
+        Optional<Vehicle> vehicle = database.findVehicleByRegistrationNumber(registrationNumber);
 
-        Vehicle vehicle = database.findVehicleByRegistrationNumber(registrationNumber);
-
-        if (isNull(vehicle) || !vehicle.isAvailable()) {
-            System.out.println("Vehicle not available");
-            return;
+        if (vehicle.isEmpty() || !vehicle.get().isAvailable()) {
+            return false;
         }
 
-        vehicles.add(vehicle);
-    }
-//Resource Leaks (Scnanners not closed)
-    public void removeItem() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the registration number of the vehicle: ");
-        String registrationNumber = scanner.nextLine();
-        vehicles.remove(new Vehicle(registrationNumber));
+        vehicles.add(vehicle.get());
+        return true;
     }
 
+    /**
+     * Remove a vehicle from the cart by registration number
+     * 
+     * @param registrationNumber the registration number of the vehicle
+     * @return true if item was removed, false if not found in cart
+     */
+    public boolean removeItem(String registrationNumber) {
+        Vehicle vehicleToRemove = new Vehicle(registrationNumber);
+        return vehicles.remove(vehicleToRemove);
+    }
+
+    /**
+     * Display the contents of the shopping cart
+     */
     public void viewCart() {
         System.out.println("\n\nShopping cart\n\n");
 
-        if(vehicles.isEmpty()) {
+        if (vehicles.isEmpty()) {
             System.out.println("Cart is empty");
             return;
         }
 
-        for (Vehicle vehicle : vehicles) {
-            System.out.println(vehicle.toString());
-        }
+        vehicles.forEach(System.out::println);
+    }
+
+    /**
+     * Get the number of items in the cart
+     */
+    public int getCartSize() {
+        return vehicles.size();
+    }
+
+    /**
+     * Clear all items from the cart
+     */
+    public void clearCart() {
+        vehicles.clear();
     }
 }
+
+//  Deleted all Scanner objects from addItem() and removeItem()
+//   Separated I/O from business logic .
+// addItem() now accepts registration number as parameter instead of reading from console
+// removeItem() now accepts registration number as parameter instead of reading from console
+// Added boolean return types
+// Added Utility Methods
+
+
